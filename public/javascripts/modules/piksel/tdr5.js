ad06e7fab15de50164ca16cc19444fa91cf5bad3
Pera <alek.br@gmail.com>
2019-10-21 08:56:58 -0700

    tdr5 wrapup

--- public/javascripts/modules/piksel/tdr5.js
+++ public/javascripts/modules/piksel/tdr5.js
@@ -25,9 +25,30 @@ class TDR5 extends Module {
 		}
 	}	
 
-	arrow(start, size, color, stroke) {
+	arrowEdge(start, size, color) {
+
 		var delta = size*0.2
-		// drawRectangleOutline(start, size, size, "rgba(255,255,255,0.2)", this.getDomID())
+
+		var pathEntries = [
+			[start[0]+size - delta, start[1] + delta], 
+			[start[0]+size - delta, start[1]+size], 
+			[start[0]+size, start[1]+size-delta], 
+			[start[0]+size, start[1]], 
+			[start[0]+delta, start[1]], 
+			[start[0], start[1]+delta], 
+			[start[0]+size-delta, start[1]+delta], 
+			]
+
+
+		path( {
+			d: "M" + pathEntries.map(x => x.join(" ")).join(" L") + "",
+			style: "fill:" + color + ";stroke:" + color + ";stroke-width:" + 1
+		}, this.getDomID())
+
+	}
+
+	arrow(start, size, color) {
+		var delta = size*0.2
 
 		var pathEntries = [
 			[start[0], start[1] + size - delta/Math.sqrt(2)], 
@@ -42,32 +63,17 @@ class TDR5 extends Module {
 			[start[0], start[1]+size-delta/Math.sqrt(2)]
 			]
 
-		// just the edge - works
-
-		// var pathEntries = [
-		// 	[start[0]+size - delta, start[1] + delta], 
-		// 	[start[0]+size - delta, start[1]+size], 
-		// 	[start[0]+size, start[1]+size-delta], 
-		// 	[start[0]+size, start[1]], 
-		// 	[start[0]+delta, start[1]], 
-		// 	[start[0], start[1]+delta], 
-		// 	[start[0]+size-delta, start[1]+delta], 
-		// 	]
-
-
 		path( {
 			d: "M" + pathEntries.map(x => x.join(" ")).join(" L") + "",
-			style: "fill:" + color + ";stroke:" + stroke + ";stroke-width:" + 1
+			style: "fill:" + color + ";stroke:" + color + ";stroke-width:" + 1
 		}, this.getDomID())
 
 	}
 
 	render() {	
-		// this.renderGrid(10,10)
-		// this.arrow([400,400],200, "#fff", "#fff")
-		// this.arrow([200,200],100, "#fff", "#fff")
+		// this.renderGrid(32,22)
 		for (var i=0; i<20; i++) {
-			this.arrow([Math.random()*xmax,Math.random()*ymax],200*Math.random(), "#fff", "#fff")
+			this.arrow([Math.random()*xmax,Math.random()*ymax],200*Math.random(), "#fff")
 		}
 	}
 
