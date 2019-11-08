fb3403c39370e8ffe1f9d546b204ed37f7f30484
Pera <alek.br@gmail.com>
2019-11-08 12:15:44 -0800

    cleanup

--- test.js
+++
@@ -1,8 +0,0 @@
-const SerialPort = require('serialport')
-const Readline = require('@serialport/parser-readline')
-const port = new SerialPort("/dev/ttys004", { baudRate: 38400 })
-
-const parser = new Readline()
-port.pipe(parser)
-
-parser.on('data', line => console.log(`> ${line}`))
