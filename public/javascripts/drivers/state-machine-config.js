c56f07c1c54b07ebc2f3cb6742bba0d3efc570dd
Pera <alek.br@gmail.com>
2019-11-09 12:37:26 -0800

    state machine transition matrix operational

--- public/javascripts/drivers/state-machine-config.js
+++ public/javascripts/drivers/state-machine-config.js
@@ -3,5 +3,10 @@
 // Configuration of active modules used by the state machine worker
 //
 
-// var modules = ["Supercon2","Drop5", "Drop3", "Drop2", "Drop1", "Virtual11"]
-var modules = ["Glitch1", "Bitfilter3", "Bitfilter2"]
+var activeModule = "Glitch1"
+
+var transition = {
+	"Glitch1": [ {"Bitfilter2": 0.9}, {"Bitfilter3": 0.5}  ],
+	"Bitfilter3": [ {"Bitfilter2": 0.01}, {"Glitch1": 0.2}  ],	
+	"Bitfilter2": [ {"Bitfilter1": 0.01}, {"Glitch1": 0.2}  ]		
+}
--- public/javascripts/drivers/state-machine.js
+++ public/javascripts/drivers/state-machine.js
@@ -1,11 +1,26 @@
 //
+// apply the transition matrix to obtain the next module
+//
+var nextModule = function() {
+	var candidates = transition[activeModule]
+	var dice = Math.random()
+	var idx = Math.floor(Math.random()*transition[activeModule].length)
+	var nextModule = Object.keys(transition[activeModule][idx])[0]
+	if (transition[activeModule][idx][nextModule] < dice) {
+		activeModule = nextModule
+		return nextModule
+	} else {
+		return activeModule
+	}
+}
+
+//
 // pull dynamically list of modules from the config file
 //
 var driverFunction = function(bc) {
-
 	importScripts('state-machine-config.js')
 	bc.postMessage(JSON.stringify({'control': 'clear-canvas'}))		
-	bc.postMessage(JSON.stringify({'control': 'set-module', 'name': modules[Math.floor(Math.random()*modules.length)]}))		
+	bc.postMessage(JSON.stringify({'control': 'set-module', 'name': nextModule()}))		
 	bc.postMessage(JSON.stringify({'control': 'start-module'}))	
 }
 
